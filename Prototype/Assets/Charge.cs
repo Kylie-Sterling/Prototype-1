26c6fec5bf19025ecb45a3874276db91cf3037f2
Elliot <elliot.newington@mail.bcu.ac.uk>
2024-05-13 18:41:38 +0100

    Update Charge.cs

--- Prototype/Assets/Charge.cs
+++ Prototype/Assets/Charge.cs
@@ -7,16 +7,23 @@ public class Charge : MonoBehaviour
     public float maxCharge;
     public float charge;
     public float depleteSpeedPerSecond;
+
+    public float damageCD;
+    public float damageValue;
+
+    public float iframeTime;
     // Start is called before the first frame update
     void Start()
     {
         charge = maxCharge;
+        damageValue = (maxCharge / 5);
     }
 
     // Update is called once per frame
     void Update()
     {
         charge -= depleteSpeedPerSecond * Time.deltaTime;
+        damageCD -= 1 * Time.deltaTime;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -26,6 +33,12 @@ public class Charge : MonoBehaviour
             PlayerBars player = FindAnyObjectByType<PlayerBars>();
             player.unit.charge = player.unit.maxCharge;
         }
+        if (other.gameObject.CompareTag("EnemyTag") && damageCD <= 0)
+        {
+            Charge c = FindAnyObjectByType<Charge>();
+                c.charge -= damageValue;
+            damageCD = iframeTime;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -34,6 +47,14 @@ public class Charge : MonoBehaviour
         {
             PlayerBars player = FindAnyObjectByType<PlayerBars>();
             player.unit.charge = player.unit.maxCharge;
+        }
+        if (collision.gameObject.CompareTag("EnemyTag") && damageCD <= 0)
+        {
+            Debug.Log("Damage");
+            Charge c = FindAnyObjectByType<Charge>();
+                c.charge -= damageValue;
+            damageCD = iframeTime;
+
         }
     }
 }
