f672f473054df16fa828a3a545eed1bda9e9b890
Elliot <elliot.newington@mail.bcu.ac.uk>
2024-05-15 18:23:03 +0100

    Added menu
    menu there

--- Prototype/Assets/PauseMenu.cs
+++ Prototype/Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -44,5 +45,10 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+    }
+
+    public void goToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
