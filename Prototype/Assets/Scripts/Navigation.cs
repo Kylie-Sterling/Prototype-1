c6977a7168b64310b5b7b88da9bf5478a7b397a2
Theo Bradley <gaminggekoman@gmail.com>
2024-05-27 17:03:50 +0100

    Added waypoint navigation -Disabled all but the first enemy in the scene because they don't have waypoints yet. -Added patrolWaypoints on the object to hold a list of all the patrol waypoint positions -Added Navigation.cs ref on the enemyAttackAnimation.cs script -Updated the prefab with this new ref
    Navigation.cs:
    -Added attacking to EnemyState enum
    -Added patrolWaypoints, currentWaypointIndex variables
    -Removed goal and stopChaseDistance variables
    -Set currentState inital state to patrolling
    -Changed AI logic to use waypoint system when patrolling
    -Added mod function that works with negative numbers
    
    EnemyAttackAnimation.cs:
    -Added navController funcion to allow the Navigation.currentState to be updated by the script
    -Added update function to check if the attack anim has finished
    -Added checks to both OnTriggerEnter and ..Exit to make sure it is only reacting to the player

--- Prototype/Assets/Scripts/Navigation.cs
+++ Prototype/Assets/Scripts/Navigation.cs
@@ -1,59 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Navigation : MonoBehaviour
 {
-
     public enum EnemyState
     {
         patrolling,
         alert,
         dead,
-        idle
+        idle,
+        attacking
     }
 
     public NavMeshAgent agent;
-    public Transform goal;
     public float stayChaseDistance; //distance at which the player is too far to chase
-    public float stopChaseDistance; //distance at which the player is too close to chase // REMOVE AND MAKE ENEMY STOP FOLLOWING WHEN ATTACKING (enemyAttackAnimation.cs)
-    //public bool toggle;
-    public EnemyState currentState; //current enemy state
-    // Start is called before the first frame update
+    public List<Vector3> patrolWaypoints; //list of patrol waypoints
+    private int currentWaypointIndex = 0; //index of currently selected waypoint
+    public EnemyState currentState = EnemyState.patrolling; //current enemy state
+    private PlayerController playerController; //basically a player ref
+
     void Start()
     {
-        //agent = GetComponent<NavMeshAgent>();
-        PlayerController p = FindAnyObjectByType<PlayerController>();
-        goal = p.gameObject.transform; //set the goal to the player initally
+        playerController = FindAnyObjectByType<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentState == EnemyState.alert) //if should chase the player
+        if (currentState != EnemyState.attacking) //if we aren't attacking
         {
-            agent.isStopped = false; //make sure agent is active
-            agent.SetDestination(goal.position); //set the navmesh target to follow the player
-        }
-        else //replace with switch
-        {
-            //agent.SetDestination(transform.position); //remove this shit, this updates the path EVERY frame for no reason
-            agent.isStopped = true; //stop following the player by telling the agent to stop
-        }
+            if (currentState == EnemyState.alert) //if should chase the player
+            {
+                agent.isStopped = false; //make sure agent is active
+                agent.SetDestination(playerController.transform.position); //set the navmesh target to follow the player
+            }
+            if (currentState == EnemyState.patrolling) //if should be patrolling
+            {
+                agent.isStopped = false; //allow agent to move
 
-
-        if (Vector3.Distance(transform.position, goal.position) > stayChaseDistance //if too far from the player
-            || Vector3.Distance(transform.position, goal.position) < stopChaseDistance) //or if too close to player
-        {
-            currentState = EnemyState.idle; //stop following player
-        }
+                if (agent.remainingDistance <= 0.05f) //if reached destination
+                {
+                    currentWaypointIndex = mod(currentWaypointIndex + 1, patrolWaypoints.Count); //increment index and wrap it
+                    agent.SetDestination(patrolWaypoints[currentWaypointIndex]); //target the next waypoint
+                }
+                else
+                { //todo: maybe remove this as it updates path every frame
+                    agent.SetDestination(patrolWaypoints[currentWaypointIndex]); //target next waypoint
+                }
+            }
         
+
+            if (Vector3.Distance(transform.position, playerController.transform.position) > stayChaseDistance) //if too far from player
+            {
+                currentState = EnemyState.patrolling; //stop following player
+            }
+        }
+
+        if (currentState == EnemyState.attacking) //if we are attacking
+        {
+            agent.isStopped = true; //stop agent
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player")) //if the player entered the trigger
+        if(other.tag == "Player") //if the player entered the trigger
         {
             currentState = EnemyState.alert; //follow
         }
@@ -61,15 +76,14 @@ public class Navigation : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") //if the player left the trigger
-            && Vector3.Distance(transform.position, goal.position) > stayChaseDistance) //and it is too far away
+            && Vector3.Distance(transform.position, playerController.transform.position) > stayChaseDistance) //and it is too far away
         {
-            currentState = EnemyState.idle; //stop following
+            currentState = EnemyState.patrolling; //stop following
         }
-        /*else
-        {
-            toggle = true;
-        }*/ //this doesnt make sense, why would we start following the player if the above statement fails?
-        //we should already be following the player, and if an object leaves the trigger that isnt the player we
-        //would start follwoing the player
+    }
+
+    private int mod(int x, int m) //modulus function
+    {
+        return (x%m + m)%m; //negative friendly function
     }
 }
--- Prototype/Assets/Scripts/enemyAttackAnimation.cs
+++ Prototype/Assets/Scripts/enemyAttackAnimation.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class enemyAttackAnimation : MonoBehaviour
 {
-    public Animator anim;
+    public Animator anim; //animator controller
+    public Navigation navController; //reference to naviagtion script
+    private bool shouldAttack = false; //should the enemy be attacking right now
+
+    private void Update()
+    { //this code resets the nav state when the attack anim finishes
+        if (anim.GetNextAnimatorStateInfo(0).IsName("enemyIdle") && shouldAttack) //if idle, but still in range
+        {
+            navController.currentState = Navigation.EnemyState.alert; //follow the player
+        }
+    }
+
     private void OnTriggerEnter(Collider other) //if an object enters the sphere trigger in front of the enemy
     {
-        anim.SetTrigger("enemyAttack"); //start attack animation
+        if (other.tag == "Player") //if player enters the attack trigger
+        {
+            anim.SetTrigger("enemyAttack"); //start attack animation
+            shouldAttack = true; //should attack
+            navController.currentState = Navigation.EnemyState.attacking; //set nav state to attacking (stop following)
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") //.. left the trigger
+        {
+            navController.currentState = Navigation.EnemyState.alert; //go back to the following state
+            shouldAttack = false; //shouldn't attack
+        }
     }
 }
